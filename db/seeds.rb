bf6bf6a4d48d38ba824e8988e6ff9085b738a123
Theo Soubra Belay <TheoSoubraBelay@users.noreply.github.com>
2026-05-20 15:47:07 +0200

    restaurant

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,39 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Restaurant.create!(
+  name: "Le Bistrot Lyonnais",
+  address: "12 Rue de la Paix, 75002 Paris",
+  phone_number: "01 42 00 00 00",
+  category: "french"
+)
+
+Restaurant.create!(
+  name: "Kebab 3000",
+  address: "15 Rue de la Paix, 75002 Paris",
+  phone_number: "01 42 02 00 00",
+  category: "belgian"
+)
+
+Restaurant.create!(
+  name: "Tong",
+  address: "16 Rue de la Paix, 75002 Paris",
+  phone_number: "01 42 02 20 00",
+  category: "chinese"
+)
+
+Restaurant.create!(
+  name: "Tong est",
+  address: "10 Rue de la Paix, 75002 Paris",
+  phone_number: "01 42 02 30 00",
+  category: "japanese"
+)
+
+Restaurant.create!(
+  name: "Pates 3000",
+  address: "9 Rue de la Paix, 75002 Paris",
+  phone_number: "01 42 02 00 30",
+  category: "italian"
+)
+
